9f10a946d2f7533a7d3d738ff8e4f6970b4f7698
Oluwasegun Ige <mailshegze@gmail.com>
2026-02-09 14:14:57 +0100

    feat: implement authentication middleware for registration

--- src/routes/EmployeeRoute.js
+++ src/routes/EmployeeRoute.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../controllers/EmployeeController');
+const authenticate = require('../middleware/authenticate');
 const authorizeRoles = require('../middleware/authorizeRoles');
 
 // POST /api/v1/employees/register
-router.post('/register', authorizeRoles('Admin'), employeeController.createEmployee);
+router.post(
+    '/register', 
+    authenticate, 
+    authorizeRoles('Admin'), 
+    employeeController.createEmployee
+);
 
 // POST /api/v1/employees/login
 router.post('/login', employeeController.loginEmployee);
