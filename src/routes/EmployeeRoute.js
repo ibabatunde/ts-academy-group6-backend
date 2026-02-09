05601b37b0b9ad03f0d828cf5de2b0f0ce95184f
Oluwasegun Ige <mailshegze@gmail.com>
2026-02-09 13:32:03 +0100

    WIP: auth middleware before merging login

--- src/routes/EmployeeRoute.js
+++ src/routes/EmployeeRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../controllers/EmployeeController');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
 // POST /api/v1/employees/register
-router.post('/register', employeeController.createEmployee);
+router.post('/register', authorizeRoles('Admin'), employeeController.createEmployee);
 
 module.exports = router;
